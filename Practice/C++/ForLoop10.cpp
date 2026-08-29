4ab4106595fe7d1a80222c7ec9a74603bcd77ae7
cchhhaaiii <yuvachaithanya.c@gmail.com>
2026-08-29 16:01:44 +0530

    Update

---
+++ Practice/C++/ForLoop10.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    cout << "The for loop with post decrement for the entered number is : ";
+    int i;
+    cin >> i;
+    for (; i <= 15; i++){
+     cout << i << endl;
+    }
+    return 0;
+}
