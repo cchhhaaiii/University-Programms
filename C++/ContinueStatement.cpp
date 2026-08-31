c1626b6eb76253187d3a13371d7de600574cb4ee
cchhhaaiii <yuvachaithanya.c@gmail.com>
2026-08-31 14:03:16 +0530

    Update

---
+++ C++/ContinueStatement.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    int i =0, num;
+    cout << "Enter an upper limit : ";
+    cin >> num;
+    while (i++ <= num)
+    {
+       if (i%5==0)
+          continue;
+    else 
+    
+        cout << i << endl;
+    }
+}
