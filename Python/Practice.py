6387b4c4059446490dbf8af97c0aa181bf56a58c
cchhhaaiii <yuvachaithanya.c@gmail.com>
2026-08-31 12:47:42 +0530

    Update

---
+++ Python/Practice.py
@@ -0,0 +1,12 @@
+print("Hello World")
+a = 12 
+b = 12.4
+c = "Hello World"
+d = True
+e = [1, 2, 3 , 4]
+print(a, b, c, d , e)
+f, g, h , i , j = 12, 12.4, "Hello World", True, [1, 2, 3 , 4]
+print(f, g, h , i , j)
+
+k = input("Enter your age: ")
+print ("Age of the person is : " ,k)
