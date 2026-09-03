838f3e3721b13d2c9ddd8efbdf34a64ad814d729
cchhhaaiii <yuvachaithanya.c@gmail.com>
2026-09-03 13:02:48 +0530

    Update

---
+++ C++/SearchElementInArray.cpp
@@ -0,0 +1,32 @@
+//program to search an element in an array taken from the user
+#include <iostream>
+using namespace std;
+
+int main (){
+    int size;
+    cout << "Enter the size of the array: ";
+    cin >> size;
+
+    int arr[size];
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < size; i++) {
+        cin >> arr[i];
+    }
+
+    int search;
+    cout << "Enter the element to search: ";
+    cin >> search;
+
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == search) {
+            cout << "Element found at index: " << i << endl;
+
+            break;
+        }
+        else if (i == size - 1) {
+            cout << "Element not found in the array." << endl;
+        }
+    }
+
+    return 0;
+}
