355b537f070b626eef1d7f9ff29fde357651cd8e
cchhhaaiii <yuvachaithanya.c@gmail.com>
2026-09-08 08:58:33 +0530

    Update

---
+++ C++/AdditionofTwoMatrices.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rows, cols;
+    cout << "Enter rows and columns: ";
+    cin >> rows >> cols;
+
+    int matrix1[10][10], matrix2[10][10], sum[10][10];
+
+    cout << "Enter first matrix elements: ";
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cin >> matrix1[i][j];
+        }
+    }
+
+    cout << "Enter second matrix elements: ";
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cin >> matrix2[i][j];
+            sum[i][j] = matrix1[i][j] + matrix2[i][j]; 
+        }
+    }
+
+    cout << "Result: ";
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cout << sum[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
